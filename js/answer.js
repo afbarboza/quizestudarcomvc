5f516e6465bc7000ab41b5ef04ffe7f3ce6d5b25
afbarboza <alex.barboza@usp.br>
2017-10-23 00:05:24 -0200

    answer mvc done

---
+++ js/answer.js
@@ -0,0 +1,251 @@
+/**
+ * getCurrentQuestionIndex - get the number of the current question
+ *
+ *
+ */
+function getCurrentQuestionIndex() {
+	var currentQuestionIdx = 0;
+	var strCurrentQuestionIdx = localStorage.getItem("currentQuestionCounter");
+	if (strCurrentQuestionIdx == null) {
+		localStorage.setItem("currentQuestionCounter", 1);
+		currentQuestionIdx = 1;	
+	} else {
+		currentQuestionIdx = parseInt(strCurrentQuestionIdx);
+	}
+	return currentQuestionIdx;
+}
+
+/**
+ * incCurrentQuestionIndex - increment the counter of questions
+ *	already alswered by the user.
+ *
+ */
+function incCurrentQuestionIndex() {
+	var currentQuestionIdx = 0;
+	var strCurrentQuestionIdx = getCurrentQuestionIndex();
+	currentQuestionIdx = parseInt(strCurrentQuestionIdx);
+	currentQuestionIdx++;
+	localStorage.setItem("currentQuestionCounter", currentQuestionIdx);
+}
+
+function finishCurrentQuiz() {
+	 localStorage.setItem("currentQuestionCounter", 1);
+	/*TODO: finish quiz code goes here... */
+}
+
+
+
+/**
+ * setCurrentQuestionNumber - set the number of the current question
+ *
+ */
+function setCurrentQuestionNumber() {
+	var questionNbr = getCurrentQuestionIndex();
+	document.getElementById("lbl_question_nbr").textContent = "Questão " +  questionNbr;
+}
+
+/**
+ * generateRandomAnswersSequence - generate some random sequence of answers.
+ *
+ */
+function generateRandomAnswersSequence() {
+	var possibleSequences  = ["1234","1243","1324","1342","1423","1432","2143","2134","2341","2314","2431","2413","3124","3142","3214","3241","3412","3421","4132","4123","4231","4213","4321","4312"];
+	var randomSequence = Math.floor(Math.random() * (23 - 0)) + 0;
+	return possibleSequences[randomSequence];
+}
+
+/**
+ * setCurrentQuestionStatement - sets the current enuciation of the question
+ *
+ */
+function setCurrentQuestionStatement() {
+	/* gets the quiz title and question number */
+	var currentUserQuiz = localStorage.getItem("currentUserQuiz");
+	var currentUserQuestionIdx = getCurrentQuestionIndex();
+
+	/* retrieves statement from localStorage */
+	var statement = localStorage.getItem(currentUserQuiz + currentUserQuestionIdx + "statement");
+
+	/*update the view */
+	document.getElementById("lbl_question_stmt").textContent = statement;
+}
+
+/**
+ * setRightAnswer - sets the right answer for the user.
+ *
+ * @randomSequence - the generated random sequence of answers
+ * @quizTitle - the name of te quiz who owns the current question (primary key)
+ * @questionID - the number of the current question
+ *
+ */
+function setRightAnswer(randomSequence, quizTitle, questionID) {
+	/* first: retrieves the right answer from local storage */
+	var rightAnswer = localStorage.getItem(quizTitle + questionID + "right_answer");
+
+	
+	/* lookup the position of the reight answer */
+	var rightAnswerPosition = 0;
+
+	/* puts the right answer in this position */
+	for (i = 0; i < 4; i++) {
+		if (randomSequence.charAt(i) == '1') {
+			rightAnswerPosition = (i + 1);
+		}
+	}
+
+	/* stores the position of the right answer */
+	localStorage.setItem("curr_right_answer", rightAnswerPosition);
+
+	/* just updates the view */
+	document.getElementById("ans" + rightAnswerPosition).textContent = rightAnswer;
+}
+
+/**
+ * setWrongAnswer - seths the label of the wrong answer
+ *
+ * @randomSequence - the generated random sequence of nswers
+ * @quizTitle - the name of the quiz who owns the current question (primary key)
+ * @questionID - the number of the current question
+ * @wans: index of the wrong answer (1 - wrong answer 1, and so on...)
+ *
+ */
+function setWrongAnswer(randomSequence, quizTitle, questionID, wans) {
+	var wrongAnswer = localStorage.getItem(quizTitle + questionID + "wrong_answer" + wans);
+
+	var wrongAnswerPosition = 0;
+	for (i = 0; i < 4; i++) {
+		if (randomSequence.charAt(i) == (wans + 1).toString()) {
+			wrongAnswerPosition = (i + 1)	
+		}
+	}
+
+	document.getElementById("ans" + wrongAnswerPosition).textContent = wrongAnswer;
+}
+
+/**
+ * checkAnswerSelection - checks whether whether a radio
+ *	was selected.
+ *
+ * return true if one radio is checked;
+ *	false, otherwise.
+ */
+function checkAnswerSelection() {
+	var check1 = document.getElementById("opt1").checked;
+	var check2 = document.getElementById("opt2").checked;
+	var check3 = document.getElementById("opt3").checked;
+	var check4 = document.getElementById("opt4").checked;
+	return (check1 || check2 || check3 || check4);
+}
+
+
+/**
+ * probeRightAnswer - checks whether the user selected the right answer
+ *
+ * return true if the user seletected the right answer
+ *	false, otherwise.
+ */
+function probeRightAnswer() {
+	/* gets the position of the right answer */
+	var rightAnswerPos = parseInt(localStorage.getItem("curr_right_answer"));
+
+	/* gets the selected element */
+	var check1 = document.getElementById("opt1").checked;
+	var check2 = document.getElementById("opt2").checked;
+	var check3 = document.getElementById("opt3").checked;
+	var check4 = document.getElementById("opt4").checked;
+
+	if (check1 == true && rightAnswerPos == 1)
+		return true;
+
+	if (check2 == true && rightAnswerPos == 2)
+		return true;
+
+	if (check3 == true && rightAnswerPos == 3)
+		return true;
+
+	if (check4 == true && rightAnswerPos == 4)
+		return true;
+
+	return false;
+}
+
+/**
+ * incUserScore - increments the score of the user.
+ *
+ * The score is incremented if, and only if, the right 
+ * answer was seletected.
+ */
+function incUserScore() {
+	/* store the current user score*/
+	var currentUserScore = 0;
+
+	/* get old score of the user */
+	var strCurrentUserScore = localStorage.getItem("curr_score");
+
+	/* if a score does not exist, we create one */
+	if (strCurrentUserScore == null) {
+		localStorage.setItem("curr_score", 0);
+	} else {
+		currentUserScore = parseInt(strCurrentUserScore);
+	}
+	currentUserScore++;
+	
+	/* stores back the incremented score*/
+	localStorage.setItem("curr_score", currentUserScore);
+}
+
+function getPercentageScoreHits() {
+	var sizeOfQuiz = parseInt(localStorage.getItem("currentUserQuizSize"));
+	var currentUserScore = parseInt(localStorage.getItem("curr_score"));
+
+	return ((currentUserScore / sizeOfQuiz) * 100);
+}
+
+$(document).ready(function() {
+	/* updates the  question number and statement */
+	setCurrentQuestionNumber();
+	setCurrentQuestionStatement();
+
+	/* gets some basic info about the current question */
+	var answerSequence = generateRandomAnswersSequence();
+	var currentQuiz = localStorage.getItem("currentUserQuiz");
+	var currentIdx = getCurrentQuestionIndex();
+
+	/* updates the answer to the question */
+	setRightAnswer(answerSequence, currentQuiz, currentIdx);
+	setWrongAnswer(answerSequence, currentQuiz, currentIdx, 1);
+	setWrongAnswer(answerSequence, currentQuiz, currentIdx, 2);
+	setWrongAnswer(answerSequence, currentQuiz, currentIdx, 3);
+
+
+	/* adds a handler to the click listener of the next button */
+	$("#next_question").click(function() {
+
+		/* checks the user selected a question */
+		if (checkAnswerSelection() == false) {
+			alert("Você deve selecionar uma resposta!");
+			return;
+		}
+		/* checks whether the user selected the right answer*/
+		if (probeRightAnswer() == true) {
+			alert("parabéns! resposta certa!");
+			incUserScore();
+		}
+
+		/* checks whether the user completed the quiz */
+		lengthOfQuiz =  parseInt(localStorage.getItem("currentUserQuizSize"));
+		if (lengthOfQuiz == getCurrentQuestionIndex()) {
+			alert("Parabéns! Você finalizou o quiz!");
+			alert("Sua porcentagem de acertos foi de " + getPercentageScoreHits() + "%");
+			finishCurrentQuiz();	
+		} else {
+			incCurrentQuestionIndex();
+			location.reload();
+		}
+	});
+
+	$("#restart_quiz").click(function() {
+		finishCurrentQuiz();
+		location.reload();
+	});
+});
--- js/dashboard.js
+++ js/dashboard.js
@@ -31,12 +31,46 @@ function getNumberOfQuizes() {
 	return value;
 }
 
-function getSelectQuizName(hrefID) {
+/**
+ * getSelectedQuizName - gets the name of the selected quiz
+ *
+ */
+function getSelectedQuizName(hrefID) {
+	return (document.getElementById(hrefID).textContent);
+}
+
+/**
+ * getIndexQuiz - gets the index of the selected quiz
+ *
+ * @quizName - the name of the quiz
+ */
+function getIndexQuiz(quizName) {
+	var numberOfQuizes = getNumberOfQuizes();
+	for (i = 1; i < (numberOfQuizes + 1); i++) {
+		var tmpQuizName = localStorage.getItem("quiz" + i);
+		if (quizName.localeCompare(tmpQuizName) == 0)
+			return i;
+	}
+	return 0;
+}
+
+function storeSelectedQuizInfo(quizTitle, quizIndex) {
+	var strCurrQuizSize = "";
+	var currQuizSize = 0;
+
+	/* save the selected quiz to be done by the user */
+	localStorage.setItem("currentUserQuiz", quizTitle);
+
+	/* save the size of the selected quiz */
+	var strCurrQuizSize = localStorage.getItem("number_questions_quiz" + quizIndex);
+	currQuizSize = parseInt(strCurrQuizSize);
+	localStorage.setItem("currentUserQuizSize", currQuizSize);
+	alert("o tamanho do quiz selecionado é de " + currQuizSize + " questoes");
 }
 
 $(document).ready(function() {
 
-	/* lits all the created quizes */
+	/* lits all the created quizes after the page is loaded */
 	var quizCounter = getNumberOfQuizes();
 	if (quizCounter == 0) {
 		alert("não há quizes criados");
@@ -56,8 +90,7 @@ $(document).ready(function() {
 		}
 	}
 
-	
-
+	/* handler to the 'create new quiz' button */
 	$("#add_quiz").click(function() {
 		var quizTitle = prompt("Dê um nome ao seu quiz", "QuizLand :) ");
 		alert("O nome do seu quiz será " + quizTitle);
@@ -67,9 +100,21 @@ $(document).ready(function() {
 		return false;
 	});
 
+	/* handler to answer a new quiz */
 	$(".li_item").click(function(e) {
+		/* gets the quiz identifier */
 		e.preventDefault();
-		
-		alert(document.getElementById(this.id).textContent);
+		var selectedQuiz = getSelectedQuizName(this.id);
+		alert("selectedQuiz: " + selectedQuiz);
+
+		/* gets the quiz index */
+		var selectedQuizIdx = getIndexQuiz(selectedQuiz);
+		alert("o indice do quiz corrente é " + selectedQuizIdx);
+
+		/* store the informations about the selectec quiz to be done */
+		storeSelectedQuizInfo(selectedQuiz, selectedQuizIdx);
+
+		/* redirects the user to the answer quiz page */
+		window.location.assign("./answer.html")	;
 	});
 });
